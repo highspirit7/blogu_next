e2142b93f2f493399deb07c24ce01ed1b6448a5a
GrownUprince <jake.lee4006@gmail.com>
2025-01-23 09:02:28 +0900

    style: apply shadow for header

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,10 +1,9 @@
-import { Search } from 'lucide-react';
-import { ThemeModeToggle } from './theme-mode-toggle';
-import { Button } from '@/components/ui/button';
+import { Search } from 'lucide-react'
+import { ThemeModeToggle } from './theme-mode-toggle'
+import { Button } from '@/components/ui/button'
 
 const Header = () => (
-  // ! Box shadow 적용이 잘 안된다
-  <header className="p-4 px-20 flex justify-between items-center sticky top-0 z-10 shadow-[0_0_8px_rgba(150,150,150,0.08)]">
+  <header className="p-4 px-20 flex justify-between items-center sticky top-0 z-10 shadow-[0_0_16px_rgba(125,125,125,0.16)]">
     <h1 className="text-3xl">GrownuPrince</h1>
     <div className="flex gap-1">
       <ThemeModeToggle />
@@ -14,6 +13,6 @@ const Header = () => (
     </div>
     {/* "shadow-[0_35px_60px_-15px_rgba(0,0,0,0.3)]" */}
   </header>
-);
+)
 
-export default Header;
+export default Header
