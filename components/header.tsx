911257c92482d5a54172b70e3340bd00d2cbb3f5
GrownUprince <jake.lee4006@gmail.com>
2025-01-28 12:25:09 +0900

    chore: use Link component on the title of header

--- components/header.tsx
+++ components/header.tsx
@@ -1,11 +1,15 @@
+import Link from 'next/link';
 import { Search } from 'lucide-react';
+
 import { ThemeModeToggle } from './theme-mode-toggle';
 import { Button } from '@/components/ui/button';
 
 const Header = () => (
   <header className="w-full p-4 fixed top-0 z-10 backdrop-blur-[4px]  shadow-[0_0_16px_rgba(125,125,125,0.16)]">
     <div className="max-w-screen-xl mx-auto flex justify-between items-center">
-      <h1 className="text-3xl">{"Laffee's blogu"}</h1>
+      <Link href="/">
+        <h1 className="text-3xl">{"Laffee's blogu"}</h1>
+      </Link>
       <div className="flex gap-1">
         <ThemeModeToggle />
         <Button variant="ghost" size="icon">
