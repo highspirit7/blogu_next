c020aa06015b1c53104c7cf1392b3d4bc8661560
GrownUprince <jake.lee4006@gmail.com>
2025-01-30 14:00:35 +0900

    chore: give more width for author intro on small screen

--- components/author-intro.tsx
+++ components/author-intro.tsx
@@ -1,8 +1,8 @@
-import Image from "next/image";
+import Image from 'next/image';
 
 const AuthorIntro = () => (
   <div className="px-4">
-    <div className="mb-4 flex items-start max-w-[67%]">
+    <div className="mb-4 flex items-start md:max-w-[67%]">
       <Image
         className="rounded-full mr-3"
         width={64}
@@ -13,7 +13,9 @@ const AuthorIntro = () => (
       <div className="flex-1">
         <p className="font-bold mb-0 text-xl">안녕하세요!</p>
         <p className="text-lg">
-        I started studying coding in my 30s. I’m someone who strives to grow by comparing myself to who I was yesterday, not to others. In addition to coding, I’m interested in reading, the environment, and history. I currently work as a web developer in Ulsan, South Korea.
+          I started studying coding in my 30s. I’m someone who strives to grow by comparing myself
+          to who I was yesterday, not to others. In addition to coding, I’m interested in reading,
+          the environment, and history. I currently work as a web developer in Ulsan, South Korea.
         </p>
       </div>
     </div>
