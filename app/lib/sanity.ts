e6776c42c0ccc6e353dfb5619df37b0862327919
GrownUprince <jake.lee4006@gmail.com>
2025-02-02 17:38:46 +0900

    Revert "chore: hide sanity project details"
    This reverts commit c1f678ea4e987df03a02f7b617184ceb741bffd7.

--- app/lib/sanity.ts
+++ app/lib/sanity.ts
@@ -1,9 +1,9 @@
 import { createClient } from 'next-sanity';
 
 const client = createClient({
-  apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION,
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
+  apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2025-01-25',
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || 'lfw619jp',
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
   useCdn: false,
 });
 
